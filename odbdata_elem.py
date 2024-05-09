0ef38259a83ba3ee44a83ecd2f1d7b27b4c1cb05
libofeng <bf_li@qq.com>
2024-05-09 23:02:21 +0800

    修改数据存储方式
    - np只能保存数据，不能保存列名

--- odbdata_elem.py
+++ odbdata_elem.py
@@ -3,7 +3,6 @@
 from odbAccess import *
 from abaqusConstants import *
 import os
-import numpy as np
 
 filename = 'sdw2D.csv'
 
@@ -21,8 +20,10 @@ stress_feild = frames.fieldOutputs['S'].getSubset(region=instance)
 
 target_stress = [stress_feild.values[elem_index].mises for elem_index in elem_ls]
 
-target_stress_np = np.array(target_stress)
-
-np.savetxt(os.path.join(os.getcwd(), filename), target_stress_np, delimiter=',', fmt='%.3f')
+simu_res_dir = os.path.join(os.getcwd(), filename)
+with open(simu_res_dir, 'w') as f:
+    f.write('Strain,Stress\n')
+    for i in range(len(elem_ls)):
+        f.write('{}\n'.format(target_stress[i]))
 
 odb.close()
