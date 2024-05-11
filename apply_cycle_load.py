9dc281ec49a6cf2b5b48054b79ef933eb0904d37
libofeng <bf_li@qq.com>
2024-05-11 11:10:23 +0800

    在apply_cycle_load文件中添加修改Step时间命令

--- apply_cycle_load.py
+++ apply_cycle_load.py
@@ -2,7 +2,14 @@ from abaqus import *
 from abaqusConstants import *
 import regionToolset
 
-time_list = range(201)
-amplitude_list = [0] + [1, 0] * 100
+#needs to be changed:thisModel, time_list, amplitude_list, timePeriod
+
+thisModel = mdb.models['Model-1']
+
+time_list = range(5)
+amplitude_list = [0] + [1, -1] * 4
 datatzip = tuple(zip(time_list, amplitude_list))
-mdb.models['Model-1'].TabularAmplitude(name='Amp-1', timeSpan=STEP, smooth=SOLVER_DEFAULT, data=datatzip)
+thisModel.TabularAmplitude(name='Amp-1', timeSpan=STEP, smooth=SOLVER_DEFAULT, data=datatzip)
+
+thisModel.StaticStep(name='Step-1', previous='Initial', timePeriod=4.0, maxNumInc=1000000, 
+                     initialInc=0.01, minInc=1e-5, maxInc=0.1)
